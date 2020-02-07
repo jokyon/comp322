2312a75679073bb8ec41318e586f7aeb49aa7e18
jokyon <joseph.olympia.710@my.csun.edu>
2020-02-06 23:25:29 -0800

    a

--- lab0.c
+++ lab0.c
@@ -3,34 +3,6 @@
 
 int input;
 
- void ascToBinary(int character, int *ones)
-    {
-        if(character == 1)
-        {
-           printf("1");
-           *ones+=1;
-           return;
-        }
-        else
-        {
-            char out;
-            if((character%2) == 0)
-            {
-                 out = '0';
-                 character = character/2;
-            }
-            else
-            {
-                 out = '1';
-                 character = character/2;
-                 *ones+=1;
-
-            }
-            ascToBinary(character, ones);
-            putchar (out);
-        }
-    }
-
 
 
 
@@ -40,7 +12,7 @@ int main(int argc, char* argv[])
 	FILE*  numFile;
 	numFile = fopen("numbers.txt", "r");
 
-	int arr[]
+	int arr[];
 
 	if (inputFile == NULL)
 	{
