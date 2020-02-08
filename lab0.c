9203cae2f598345543ce56b4f9056931eefa7e3b
jokyon <joseph.olympia.710@my.csun.edu>
2020-02-07 20:09:00 -0800

    read file and check parity

--- lab0.c
+++ lab0.c
@@ -1,31 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int input;
-
-
-
-
 int main(int argc, char* argv[])
 {
-	 
-	FILE*  numFile;
-	numFile = fopen("numbers.txt", "r");
-
-	int arr[];
-
-	if (inputFile == NULL)
+	int i;
+	int size;
+	//char buf[10];
+	char* buf;
+	FILE* inputFile = fopen("numbers.txt", "r");
+	if(inputFile == NULL)
 	{
-        printf("no file\n");
-        return 0;
-    }
-
-    int n;
-	while (fscanf(inputFile, " %d", &n) == 1) 
-	{
-    	printf("%d\n", n);
+		printf("error opening file");
+		exit(1);
 	}
+	fseek(inputFile, 0L, SEEK_END);
+	size = ftell(inputFile);
+	rewind(inputFile);
 
-	return 0;
+	buf = calloc(1, size +1);
+	if(!buf) fclose(inputFile),fputs("memory alloc fails",stderr),exit(1);
+	int parity;
+	if(1 != fread(buf, size, 1 , inputFile))fclose(inputFile),free(buf),fputs("entire read fails",stderr),exit(1);
+	
+		for(i= 0; i <= size+1; i++)
+		{	
+			printf("%c", buf[i]);
+			if(buf[i] == '1')
+			{
+				parity++;
+			}
+			if(buf[i] == ' ')
+			{
+				printf(" ");
+				if(parity % 2 == 0)
+				{
+					printf("even");
+					parity = 0;
+				}
+				else
+				{
+					printf("odd");
+					parity =0;
+				}
 
+				printf("\n");
+				
+			}
+
+				
+		}
+		
+	
+	fclose(inputFile);
+	free(buf);
 }
