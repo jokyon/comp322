a6102ceeef8b51ad203f0afe38715216e7016241
jokyon <joseph.olympia.710@my.csun.edu>
2020-02-07 21:18:16 -0800

    fixing read fail

--- lab0/lab0.c
+++ lab0/lab0.c
@@ -7,7 +7,7 @@ int main(int argc, char* argv[])
 	int size;
 	//char buf[10];
 	char* buf;
-	FILE* inputFile = fopen("numbers.txt", "r");
+	FILE* inputFile = fopen("./lab0/numbers.txt", "r");
 	if(inputFile == NULL)
 	{
 		printf("error opening file");
@@ -49,7 +49,7 @@ int main(int argc, char* argv[])
 
 				
 		}
-		
+	//test edit
 	
 	fclose(inputFile);
 	free(buf);
