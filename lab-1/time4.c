36d9de652fae97c1d55dc2476f1faeb70d14fbc7
jokyon <joseph.olympia.710@my.csun.edu>
2020-02-22 15:05:11 -0800

    add changes and makefile

--- lab-1/time4.c
+++ lab-1/time4.c
@@ -2,18 +2,49 @@
 #include <sys/types.h> 
 #include <unistd.h> 
 #include <time.h> 
+
+#include <sys/wait.h>
 int main() 
 
 { 
-  
+	time_t seconds; 
+    seconds = time(NULL); 
+
+    printf("start time: %ld\n", seconds);
+    
+    pid_t parent, child;
+    
+
+
+    printf("PPID: %d, PID: %d",getppid(),getpid());
+    printf("\n");
+
+    //pid_t child;
+
     // make two process which run same 
     // program after this instruction 
+    
     fork(); 
-    time_t seconds; 
-      
-    seconds = time(NULL); 
-    printf("%ld\n", seconds);
+    child = wait(NULL);
+    if(child >0)
+    {
+    	printf("child stuff\n" );
+    }
+    else if(child =0 )
+    {
+    	printf("parent stuff\n");
+    }
+    else
+    {
+    	printf("error\n");
+    }
+
+
+    printf("Parent pid = %d\n", getpid()); 
+    printf("Child pid = %d\n", child); 
+    time_t seconds2 = time(NULL); 
+    printf("%ld\n", seconds2);
   
-    printf("Hello world!\n"); 
+   
     return 0; 
 } 
