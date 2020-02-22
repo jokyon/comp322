e6c624672f2845af374de77d6aeb263f396efc0b
jokyon <joseph.olympia.710@my.csun.edu>
2020-02-22 11:06:10 -0800

    add

--- lab-1/time4.c
+++ lab-1/time4.c
@@ -1,1 +1,17 @@
-
+#include <stdio.h> 
+#include <sys/types.h> 
+#include <unistd.h> 
+int main() 
+{ 
+  
+    // make two process which run same 
+    // program after this instruction 
+    fork(); 
+    time_t seconds; 
+      
+    seconds = time(NULL); 
+    printf("%ld\n", seconds);
+  
+    printf("Hello world!\n"); 
+    return 0; 
+} 
