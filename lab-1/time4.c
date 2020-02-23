2c542c67d70f4bfc72ba2c1f1ee6195fde990c1d
jokyon <joseph.olympia.710@my.csun.edu>
2020-02-22 21:26:05 -0800

    figure out waitpid

--- lab-1/time4.c
+++ lab-1/time4.c
@@ -2,35 +2,39 @@
 #include <sys/types.h> 
 #include <unistd.h> 
 #include <time.h> 
-
 #include <sys/wait.h>
+#include <sys/times.h>
+		/*
+		struct tms 
+		{
+		    clock_t tms_utime;   //user time 
+		    clock_t tms_stime;  //system time 
+		    clock_t tms_cutime; // user time of children 
+		    clock_t tms_cstime; / system time of children 
+		};
+		*/
 int main() 
+{ 	
+	time_t seconds;
+	time(&seconds); 
+	printf("start: %ld\n", seconds); 
+	
+	clock_t times(struct tms *buf);
+	
 
-{ 
-	time_t seconds; 
-    seconds = time(NULL); 
+	pid_t parent, child;
+    printf("PPID: %d, PID: %d\n",getppid(),getpid());
 
-    printf("start time: %ld\n", seconds);
     
-    pid_t parent, child;
-    
+    fork();
 
-
-    printf("PPID: %d, PID: %d",getppid(),getpid());
-    printf("\n");
-
-    //pid_t child;
-
-    // make two process which run same 
-    // program after this instruction 
-    
-    fork(); 
-    child = wait(NULL);
-    if(child >0)
+    struct tms process; 
+  
+     if( waitpid(getpid(),NULL, WUNTRACED) >0)
     {
     	printf("child stuff\n" );
     }
-    else if(child =0 )
+    else if(waitpid(getpid(),NULL, WUNTRACED) == 0 )
     {
     	printf("parent stuff\n");
     }
@@ -40,11 +44,16 @@ int main()
     }
 
 
-    printf("Parent pid = %d\n", getpid()); 
-    printf("Child pid = %d\n", child); 
-    time_t seconds2 = time(NULL); 
-    printf("%ld\n", seconds2);
-  
-   
+
+
+    printf("user: %ld, sys: %ld\ncuser: %ld, csys: %ld\n", 
+    	process.tms_utime, process.tms_stime, process.tms_cutime, process.tms_cstime);
+    
+
+    //printf("PPID: %d, PID: %d\n",getppid(),getpid());
+    //printf("\n");   
+  	
+  	time(&seconds); 
+  	printf("stop %ld\n",seconds);
     return 0; 
 } 
