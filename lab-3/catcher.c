f871c33cd6fae49e44173ca6fdc3630a4f480066
jokyon <joseph.olympia.710@my.csun.edu>
2020-03-19 19:00:15 -0700

    creating file

--- lab-3/catcher.c
+++ lab-3/catcher.c
@@ -5,13 +5,20 @@
 #include <stdlib.h>
 #include <signal.h>
 
+void handle_sigint(int sig) 
+{ 
+    printf("Caught signal %d\n", sig); 
+} 
+  
+
+
 int main(int argc, char *argv[])
 {
 	//execve(argv[1], argv, NULL);
 	signal(SIGINT, handle_sigint); 
     while (1) 
     { 
-        printf(“hello world\n”); 
+        printf("hello world\n"); 
         sleep(1); 
     } 
     return 0; 
