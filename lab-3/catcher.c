de5e5c98f88ac80e6c54d5c64a0c7c8c9e76f00f
jokyon <joseph.olympia.710@my.csun.edu>
2020-03-23 13:12:47 -0700

    needs to catch multiple signals

--- lab-3/catcher.c
+++ lab-3/catcher.c
@@ -4,22 +4,84 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <string.h>
 
-void handle_sigint(int sig) 
+//	cd /mnt/c/Users/Joseph/comp322/lab-3  
+pid_t PID;
+
+void handler(int sig) 
 { 
-    printf("Caught signal %d\n", sig); 
+	//pid_t PID = getpid();
+    //printf("Caught signal %d\n", sig);
+    //write(STDOUT_FILENO, "SIGINT caught\n", 13);
+    if (sig == SIGTERM)
+    {
+       //printf("Received SIGUSR1!\n");
+    	write(STDOUT_FILENO, "Received SIGTERM\n", 17);
+    }
+
+	if (sig == SIGUSR1)
+    {
+       //printf("Received SIGUSR1!\n");
+    	write(STDOUT_FILENO, "Received SIGUSR1\n", 17);
+    }
+
+
+    if (sig == SIGUSR2)
+    {
+       //printf("Received SIGUSR1!\n");
+    	write(STDOUT_FILENO, "Received SIGUSR2\n", 17);
+    }
+    //exit(EXIT_SUCCESS);
 } 
   
 
 
 int main(int argc, char *argv[])
 {
+	char* TERM = "TERM";
+	char* USR1 = "USR1";
+	char* USR2 = "USR2";
+
+	
+	for(int i = 1; i <= argc; i++)
+	{
+		printf("%d\n", getpid());
+		if(strcmp(argv[i], TERM) ==0)
+		{
+			signal(SIGTERM, handler);
+			raise(SIGTERM);
+			pause();
+			//kill(getpid(), SIGTERM);
+		}
+		if(strcmp(argv[i], USR1)==0)
+		{
+			signal(SIGUSR1, handler);
+			raise(SIGUSR1);
+			pause();
+			//kill(getpid(), SIGUSR1);
+		}
+		if(strcmp(argv[i], USR2)==0)
+		{
+			signal(SIGUSR2, handler);
+			raise(SIGUSR2);
+			pause();
+			//kill(getpid(), SIGUSR2);
+		}
+
+	}
+
 	//execve(argv[1], argv, NULL);
-	signal(SIGINT, handle_sigint); 
-    while (1) 
-    { 
-        printf("hello world\n"); 
-        sleep(1); 
-    } 
+	//signal(SIGINT, handler);
+	//pause();
+	//kill(PID, SIGINT);
+	//signal(SIGUSR1, handler);
+	
+	//PID = getpid();
+    //while (1) 
+    //{ 
+        printf("cycle: %d\n", PID); 
+        //sleep(1); 
+    //} 
     return 0; 
 }
