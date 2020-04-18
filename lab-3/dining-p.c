2edb0adc9ef8badeb8d810745a0f639e196f6ea1
Joseph Olympia <60299300+jokyon@users.noreply.github.com>
2020-04-17 20:29:09 -0700

    Create dining-p.c

---
+++ lab-3/dining-p.c
@@ -0,0 +1,11 @@
+#include <fcntl.h>           /* For O_* constants */
+#include <sys/stat.h>        /* For mode constants */
+#include <semaphore.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <errno.h>
+
+sem_t * returnVal;
+sem_t * chopstick[3];
+
+
