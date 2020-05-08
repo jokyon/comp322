9bb42c7dda9c6b380cf8f91b1d3267845383e66d
jokyon <joseph.olympia.710@my.csun.edu>
2020-05-08 14:52:34 -0700

    testing moles

---
+++ lab-6/moles.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
+#include <fcntl.h>
+#include <time.h>
+#include <syslog.h>
+#include <sys/resource.h>
+#include <signal.h>
+
+// cd /mnt/c/Users/Joseph/comp322/lab-6 
+
+int main(int argc, char *argv[])
+{
+
+	FILE *fp= NULL;
+	//int j;
+
+	fp = fopen ("lab6.log", "a+");
+	if(fp == NULL)
+	{
+		printf("log fail\n");
+	}
+	fprintf(fp, "TEST Pop %s\n", argv[0]); //
+
+    //for (j = 0; j < argc; j++)
+    
+	printf("went to mole\n");
+    exit(EXIT_SUCCESS);
+}
