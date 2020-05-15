d37b90930c822de000ed9dc9727df2a68e95c495
jokyon <joseph.olympia.710@my.csun.edu>
2020-05-15 09:56:22 -0700

    random matrix

--- lab-7/matrix_gen.c
+++ lab-7/matrix_gen.c
@@ -1,1 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <time.h>
+#include <aio.h>
+#include <math.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
+// cd /mnt/c/Users/Joseph/comp322/lab-7 
+
+int main(int argc, char* argv[])
+{		
+	
+	int size = atoi(argv[1]);
+	//blocks = atoi(argv[2]);
+	int mtx[size][size];
+	srand(time(0));
+	for(int i =1; i <= size; i++)
+  	{
+  		
+  		for(int j = 1; j <= size; j++)
+  		{
+  			
+  			int mtx_value = (rand() % (100 - 0 + 1)) + 1;
+  			int negative = (rand() % (2 - 1 + 1)) + 1;
+			if(negative ==2 ) //randomly decides negative
+			{
+	   			mtx_value *= -1;
+	   		}
+  			mtx[i][j] = mtx_value;
+  			//char snum[5];
+
+  			fprintf(stdout, "%d ", mtx_value);
+  		}
+  		fprintf(stdout,"\n");
+  	}	
+ 	
+ 	return 0;
+}
